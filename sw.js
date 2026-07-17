a7780e8f2e772dae3b3f83e3a442b0cf78a97782
kinblay <kinblay@gmail.com>
2026-07-17 18:00:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'canconet-v53';
+const CACHE = 'canconet-v54'; // v54: fix any undefined (nivell 3 guess-year + bonus decada)
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
